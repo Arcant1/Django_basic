aab492d704550f8b24ff42482e5e07039aa2940a
Arcant1 <antonioarcuri77@gmail.com>
2018-09-11 17:45:58 -0300

    parte 6

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,8 @@ def index(request):
     num_instancias= Instancia.objects.all().count()
     num_instancias_disponibles=Instancia.objects.filter(estado__exact='a').count() #WARDA
     num_autores= Autor.objects.all().count()
+    num_generos = Genero.objects.all().count()
+    num_libros_darya = Libro.objects.filter(titulo__icontains='Darya').count() #isensitive case
     return render(
         request,
         'index.html',
@@ -17,6 +19,8 @@ def index(request):
             'num_libros':num_libros,
             'num_instancias':num_instancias,
             'num_instancias_disponibles':num_instancias_disponibles,
-            'num_autores':num_autores
+            'num_autores':num_autores,
+            'num_generos':num_generos,
+            'num_libros_darya':num_libros_darya,
             },
     )
